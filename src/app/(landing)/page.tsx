59e34571bd444ad718d7fbee4911772afaa4fd66
ajeet <ajeet.tumuluru@gmail.com>
2025-01-25 22:24:49 +0530

    ft: update hero section particles

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,12 +5,26 @@ import HeroSection from "@/components/landing-page/HeroSection";
 import Navbar from "@/components/landing-page/Navbar";
 import Testimonials from "@/components/landing-page/Testimonials";
 import FeaturesSectionDemo from "@/components/landing-page/Features";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex-1 flex flex-col bg-black text-white scroll-smooth">
       <Navbar />
       <HeroSection />
+      <div className="flex justify-center mb-10">
+        <Link href="/form-editor">
+          <Button
+            variant="secondary"
+            className="h-[50px] w-[200px] group text-lg font-semibold"
+          >
+            Get Started
+            <ArrowRight className="size-5 group-hover:translate-x-4 transition-all duration-300" />
+          </Button>
+        </Link>
+      </div>
       <div id="about" className="flex-1">
         <div className="mx-auto relative">
           <div className="text-5xl text-center mb-4 italic">Features</div>
